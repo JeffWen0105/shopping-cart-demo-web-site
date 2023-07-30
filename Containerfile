134819058c0c974a7edafcfc59aa94dfe3934c90
文軍皓 <58615837+JeffWen0105@users.noreply.github.com>
2023-07-30 20:34:16 +0800

    update

---
+++ Containerfile
@@ -0,0 +1,5 @@
+FROM docker.io/library/nginx:alpine
+LABEL authors="https://how64bit.com"
+COPY ./shopping-cart-demo/  /usr/share/nginx/html/
+EXPOSE 80
+CMD ["nginx", "-g", "daemon off;"]
---
+++ shopping-cart-demo/js/products.js
@@ -0,0 +1,74 @@
+const products = [
+    {
+        id: '1',
+        title: '產品一',
+        price: 10,
+        img: 'https://storage.googleapis.com/eample_0730/IMAGE/001.jpg',
+        tags: ['生活用品', '工具'],
+        isAvailable: true
+    },
+    {
+        id: '2',
+        title: '產品二',
+        price: 60,
+        img: 'https://storage.googleapis.com/eample_0730/IMAGE/002.jpg',
+        tags: ['藥妝'],
+        isAvailable: true
+    },
+    {
+        id: '3',
+        title: '產品三',
+        price: 180,
+        img: 'https://storage.googleapis.com/eample_0730/IMAGE/003.jpg',
+        tags: ['食品'],
+        isAvailable: true
+    },
+    {
+        id: '4',
+        title: '產品四',
+        price: 220,
+        img: 'https://storage.googleapis.com/eample_0730/IMAGE/004.jpg',
+        tags: ['生活用品'],
+        isAvailable: true
+    },
+    {
+        id: '5',
+        title: '產品五',
+        price: 360,
+        img: 'https://storage.googleapis.com/eample_0730/IMAGE/005.jpg',
+        tags: ['工具'],
+        isAvailable: true
+    },
+    {
+        id: '6',
+        title: '產品六',
+        price: 360,
+        img: 'https://storage.googleapis.com/eample_0730/IMAGE/006.jpg',
+        tags: ['食品'],
+        isAvailable: true
+    },
+    {
+        id: '7',
+        title: '產品七',
+        price: 400,
+        img: 'https://storage.googleapis.com/eample_0730/IMAGE/007.jpg',
+        tags: ['生活用品', '工具'],
+        isAvailable: true
+    },
+    {
+        id: '8',
+        title: '產品八',
+        price: 450,
+        img: 'https://storage.googleapis.com/eample_0730/IMAGE/008.jpg',
+        tags: ['生活用品', '工具'],
+        isAvailable: true
+    },
+    {
+        id: '9',
+        title: '產品九',
+        price: 5200,
+        img: 'https://storage.googleapis.com/eample_0730/IMAGE/009.jpg',
+        tags: ['藥妝'],
+        isAvailable: true
+    }
+];
